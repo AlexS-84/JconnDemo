23cda498105ff1504b92dbd289cdd7f567208739
AlexS-84 <sergei_alekseev@bk.ru>
2022-05-22 20:50:38 +0500

    v 22.05-2

--- src/main/java/ru/sergeysjob/Main.java
+++ src/main/java/ru/sergeysjob/Main.java
@@ -13,10 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner input = new Scanner(System.in);
+        String password;
+        System.out.print("Ведите пароль Базы данных\n");
+        password = input.nextLine();
+
         try {
             String url = "jdbc:mysql://localhost/jconndemo";
             String username = "root";
-            String password = "test1234";
             //Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection conn = DriverManager.getConnection(url,username,password)) {
                 System.out.println("Успешное подключение, Друг!");
@@ -24,8 +28,8 @@ public class Main {
                 Statement statement = conn.createStatement();
                 ResultSet resultSet = statement.executeQuery ("select * from boys");
             while (resultSet.next()){
-                String name = resultSet.getString(1);
-                String city = resultSet.getString(2);
+                String name = resultSet.getString("name");
+                String city = resultSet.getString("city");
                 System.out.printf("%s - %s\n", name, city);
             }
             }
